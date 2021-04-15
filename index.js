5049d73f2eb1ffc8fdaad79402bfd831eebefe82
Michael Alao <contactleomax@gmail.com>
2021-04-15 16:10:58 +0000

    Updated the index file to make new directory

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const content = 'Some content just created!'
 
-fs.writeFile(path.join(__dirname,'/holder','names.json'), content, err => {
+// Make a new directory
+fs.mkdir('results/names',{recursive:true},(err) => {
+  if(err){
+throw err;
+  }
+  });
+
+// Create and write to new file
+fs.writeFile(path.join(__dirname,'results/names','names.json'), content, err => {
   if (err) {
     console.error(err)
     return
   }
   //file written successfully
   console.log('File created successfully.')
-})
+});
