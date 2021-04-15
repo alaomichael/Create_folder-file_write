235225e19899ab642aec8b66179b264b58373fed
Michael Alao <contactleomax@gmail.com>
2021-04-15 16:54:51 +0000

    update index file with test data

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const fs = require('fs')
 const path = require('path')
-const content = 'Some content just created!'
+
+const url = 'http://jsonplaceholder.typicode.com/posts';
+  
+
+let posts =[ {
+  "name": "Michael Alao",
+  "age": 35,
+  "gender": "male"
+},
+ {
+  "name": "Esther Alao",
+  "age": 35,
+  "gender": "female"
+}
+]
+console.log(posts);
 
 // Make a new directory
-fs.mkdir('results/names',{recursive:true},(err) => {
+fs.mkdir('result',{recursive:true},(err) => {
   if(err){
 throw err;
   }
   });
 
 // Create and write to new file
-fs.writeFile(path.join(__dirname,'results/names','names.json'), content, err => {
+fs.writeFile(path.join(__dirname,'result','posts.json'), posts, err => {
   if (err) {
     console.error(err)
     return
