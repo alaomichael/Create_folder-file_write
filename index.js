9545470eb9ca9f21f5774e46d2f765208168a39a
Michael Alao <contactleomax@gmail.com>
2021-04-18 00:34:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 const fetch = require('node-fetch');
 
-const posters = fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(json => {
-        console.log("First user in the array:");
-        console.log(json[0]);
-        console.log("Title of the first user in the array:");
-        console.log(json[0].title);
-})
-
-
+// Api
 const url = 'http://jsonplaceholder.typicode.com/posts';
   
-
-// let posts =[ {
-//   "name": "Michael Alao",
-//   "age": 35,
-//   "gender": "male"
-// },
-//  {
-//   "name": "Esther Alao",
-//   "age": 35,
-//   "gender": "female"
-// }
-// ]
-// console.log(posts);
-
-// api url
-// const api_url = 
-//       "https://employeedetails.free.beeceptor.com/my/api/path";
-  
-// // Defining async function
-// async function getapi(url) {
-    
-//     // Storing response
-//     const response = await fetch(url);
-    
-//     // Storing data in form of JSON
-//     var data = await response.json();
-//     console.log(data);
-//   }
-// // Calling that async function
-// getapi(api_url);
-
-const newPosts = fetch(url)
+const posts = fetch(url)
 .then((resp) => resp.json())
 .then(function(data) {
   let posts = data.results;
@@ -55,6 +16,9 @@ const newPosts = fetch(url)
   console.log(error);
 });
 
+console.log(`Result from placeholder: ${posts}`);
+
+
 // Make a new directory
 fs.mkdir('result',{recursive:true},(err) => {
   if(err){
@@ -63,7 +27,8 @@ throw err;
   });
 
 // Create and write to new file
-fs.writeFile(path.join(__dirname,'result','posts.json'), posters, err => {
+const jsonString = JSON.stringify(posts,null,2);
+fs.writeFile(path.join(__dirname,'result','posts.json'), jsonString, err => {
   if (err) {
     console.error(err)
     return
@@ -71,3 +36,60 @@ fs.writeFile(path.join(__dirname,'result','posts.json'), posters, err => {
   //file written successfully
   console.log('File created successfully.')
 });
+
+ const HOST = 'localhost';
+const PORT = process.env.PORT || 5001;
+
+// Helper function
+function jsonReader(filepath,cb) {
+  fs.readFile(filepath,(err,fileData)=> {
+    if(err){
+      return cb && cb(null,object);
+    }
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null,object); 
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+
+// Calling the helper function
+// jsonReader('result/posts.json',(err,posts)=> {
+//   if (err){
+//     console.log(err);
+//     return;
+//   }
+//   console.log(posts[0].name);
+// });
+
+
+// Create a server with the HTTP library
+http.createServer((req,res) =>{
+// fs.readFile('result/posts.json', (err,data) =>{
+//   if(err) throw err;
+//   res.writeHead(200,{'Content-Type': 'application/json'});
+//   res.write(data);
+//   res.end();
+// })
+
+// Calling the helper function
+jsonReader('result/posts.json',(err,posts)=> {
+  if (err){
+    console.log(err);
+    return;
+  }
+  res.writeHead(200,{'Content-Type': 'application/json'});
+  posts= JSON.stringify(posts,null,2);
+  res.write(posts);
+  res.end();
+  console.log(`Result served to host ${posts}`);
+});
+
+
+}).listen(PORT);
+
+
+// server.listen(PORT,HOST);
+console.log(`Server running at ${HOST} on port ${PORT} 🔥`);
